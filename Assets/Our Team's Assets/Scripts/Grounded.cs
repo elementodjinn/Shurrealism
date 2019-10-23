6588158f462a48c57858d3f0d7a6d3b86375807c
Jessica Sill <jsill@uci.edu>
2019-10-22 18:04:45 -0800

    added player movement script and script to check if player is grounded; 2 player prefabs added; jess_scene has been created and has a working player prefab in it

---
+++ Assets/Our Team's Assets/Scripts/Grounded.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+
+    GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets the parent of the object the Grounded script is on
+        // and assigns it to Player
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // If Player is touching the ground, isGrounded is set to True
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<Player_Movement>().isGrounded = true;
+        }
+    }
+
+    // If player stops touching the ground, isGrounded is set to False
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<Player_Movement>().isGrounded = false;
+        }
+    }
+
+}
---
+++ Assets/Our Team's Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // horizontal move speed for the player
+    public float moveSpeed = 5f;
+
+    // vertical jump velocity for the player
+    // making it larger will cause jumps to be higher
+    public float jumpVelocity = 5f;
+
+    // gravity multiplier for high jumps (holding down the spacebar)
+    // making it larger will cause high jumps to fall more quickly
+    public float fallMultiplier = 2.5f;
+    // gravity multiplier for low jumps (single tap)
+    // making it larger will cause low jumps to be lower
+    public float lowJumpMultiplier = 2f;
+
+    // rigidbody of the Player
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // True if the player is touching the ground
+    public bool isGrounded = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the Player is falling, apply gravity multiplier
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        // if the Player is jumping but not holding the Jump button, apply low jump gravity multiplier
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        Jump();
+
+        // gets the location of the player and updates it constantly
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        
+    }
+
+    // if the Jump button is pushed, apply jump velocity
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+        
+    }
+}
