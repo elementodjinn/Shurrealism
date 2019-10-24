8bf500a729fd46c5aa2cd4b706e5b04798234533
Illrin <kelvin.mai101@yahoo.com>
2019-10-24 13:07:37 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/MovinPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovinPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    double travelTime = 5f;
+    float initialX;
+    float initialY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+        rb.velocity = new Vector2(.2f, .2f);
+        initialX = rb.position.x;
+        initialY = rb.position.y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        travelTime -= Time.deltaTime;
+        if(travelTime <= 0)
+        {
+            travelTime = Random.Range(0f, 10f);
+            rb.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            rb.transform.position = new Vector2(initialX, initialY);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    double timer = 3f;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = 3f;
+            rb.rotation = Random.Range(0,359);
+        }
+    }
+}
