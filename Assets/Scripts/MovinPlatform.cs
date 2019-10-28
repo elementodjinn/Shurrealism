25ead1d399a1e2ec8edb346026b239bbaa8b4cbc
Illrin <kelvin.mai101@yahoo.com>
2019-10-27 17:15:53 -0700

    Platform Updates

--- Assets/Scripts/MovinPlatform.cs
+++ Assets/Scripts/MovinPlatform.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class MovinPlatform : MonoBehaviour
 {
     Rigidbody2D rb;
-    double travelTime = 5f;
+    public float maxTime = 5f;
+    float travelTime;
+    public float speedScaleX = 1f; //ranges, explicit speed if random is off
+    public float speedScaleY = 1f;
+    public bool rand = true;
     float initialX;
     float initialY;
     // Start is called before the first frame update
     void Start()
     {
+        travelTime = maxTime;
         rb = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
-        rb.velocity = new Vector2(.2f, .2f);
+        if (rand) rb.velocity = new Vector2(Random.Range(-speedScaleX, speedScaleX), Random.Range(-speedScaleY, speedScaleY));
+        else rb.velocity = new Vector2(speedScaleX, speedScaleY);
         initialX = rb.position.x;
         initialY = rb.position.y;
     }
@@ -20,11 +26,18 @@ public class MovinPlatform : MonoBehaviour
     // Update is called once per frame
     void Update() {
         travelTime -= Time.deltaTime;
-        if(travelTime <= 0)
-        {
-            travelTime = Random.Range(0f, 10f);
-            rb.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            rb.transform.position = new Vector2(initialX, initialY);
+        if (travelTime <= 0){
+            if (rand)
+            {
+                travelTime = Random.Range(0f, maxTime);
+                rb.velocity = new Vector2(Random.Range(-speedScaleX, speedScaleX), Random.Range(-speedScaleY, speedScaleY));
+                rb.transform.position = new Vector2(initialX, initialY);
+            }
+            else
+            {
+                travelTime = maxTime;
+                rb.velocity = new Vector2(-rb.velocity.x, -rb.velocity.y);
+            }
         }
     }
 }
