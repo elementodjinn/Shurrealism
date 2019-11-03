8ccb4f155173a8c32d9915c9229b25c53fd2e6e2
Illrin <kelvin.mai101@yahoo.com>
2019-11-03 08:46:58 -0800

    Added Enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // horizontal move speed for the player
+    public float moveSpeed = 5f;
+
+    // gravity multiplier for high jumps (holding down the spacebar)
+    // making it larger will cause high jumps to fall more quickly
+    public float fallMultiplier = 2.5f;
+
+    // rigidbody of the Player
+    Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // True if the player is touching the ground
+    public bool leftGrounded = false;
+    public bool rightGrounded = false;
+
+    public int dir = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!leftGrounded && !rightGrounded) dir = 0;
+        else if (leftGrounded && rightGrounded)
+        {
+            if (!GetComponent<SpriteRenderer>().flipX) dir = 1;
+            else dir = -1;
+        }
+        if ((dir > 0 && !rightGrounded) || (dir < 0 && !leftGrounded))
+        {
+            dir = -dir;
+            if (dir > 0) GetComponent<SpriteRenderer>().flipX = false;
+            else GetComponent<SpriteRenderer>().flipX = true;
+        }
+        // if the Player is falling, apply gravity multiplier
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        // gets the location of the player and updates it constantly
+        Vector3 movement = new Vector3(dir, 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+    }
+}
---
+++ Assets/Scripts/sideGroundCheck.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sideGroundCheck : MonoBehaviour
+{
+    GameObject enemy;
+    public bool isLeft = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets the parent of the object the Grounded script is on
+        // and assigns it to enemy
+        enemy = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // If enemy is touching the ground, isGrounded is set to True
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (isLeft) enemy.GetComponent<EnemyMovement>().leftGrounded = true;
+            else enemy.GetComponent<EnemyMovement>().rightGrounded = true;
+        }
+    }
+
+    // If enemy stops touching the ground, isGrounded is set to False
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (isLeft) enemy.GetComponent<EnemyMovement>().leftGrounded = false;
+            else enemy.GetComponent<EnemyMovement>().rightGrounded = false;
+        }
+    }
+}
