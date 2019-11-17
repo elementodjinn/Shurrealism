a9a9a4a0a17305b3c82fdd888f9658632fe3df41
jessicasill <jsill@uci.edu>
2019-11-17 15:27:18 -0800

    added basic attack, currently only works in one direction. no animations yet

---
+++ Assets/Our Team's Assets/Scripts/AttackTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.isTrigger != true && col.CompareTag("Enemy"))
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Our Team's Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private bool attacking = false;
+
+    private float attackTimer = 0;
+    private float attackCd = 0.3f;
+
+    public Collider2D attackTrigger;
+
+    private void Awake()
+    {
+        attackTrigger.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("f") && !attacking)
+        {
+            attacking = true;
+            attackTimer = attackCd;
+
+            attackTrigger.enabled = true;
+        }
+        if (attacking)
+        {
+            if (attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                attacking = false;
+                attackTrigger.enabled = false;
+            }
+        }
+    }
+}
