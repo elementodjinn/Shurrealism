a9140759e8ba093167f5eafb09aa9846f989f9d0
Jessica Sill <jsill@uci.edu>
2019-12-01 20:48:32 -0800

    added breakable blocks

--- Assets/Our Team's Assets/Scripts/AttackTrigger.cs
+++ Assets/Our Team's Assets/Scripts/AttackTrigger.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AttackTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.isTrigger != true && (col.CompareTag("Enemy") || col.CompareTag("Wall")))
+        if (col.isTrigger != true && col.CompareTag("Enemy"))
         {
             col.gameObject.GetComponent<EnemyHealth>().takeDamage(1);
+        }
+        if (col.isTrigger != true && col.CompareTag("PunchableBlock"))
+        {
+            Console.WriteLine(col.tag);
+            Destroy(col.gameObject);
         }
     }
 }
