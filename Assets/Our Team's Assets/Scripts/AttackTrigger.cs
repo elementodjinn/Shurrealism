dd5fcb11f55130cc46e1def172b933a3a2a97a4f
Jessica Sill <jsill@uci.edu>
2019-12-01 17:16:06 -0800

    added camera movement to level

--- Assets/Our Team's Assets/Scripts/AttackTrigger.cs
+++ Assets/Our Team's Assets/Scripts/AttackTrigger.cs
@@ -6,7 +6,7 @@ public class AttackTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.isTrigger != true && col.CompareTag("Enemy"))
+        if (col.isTrigger != true && (col.CompareTag("Enemy") || col.CompareTag("Wall")))
         {
             col.gameObject.GetComponent<EnemyHealth>().takeDamage(1);
         }
